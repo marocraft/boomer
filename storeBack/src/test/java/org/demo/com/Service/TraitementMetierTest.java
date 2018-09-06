4f505d68c6336780b37501ec8b1f09edfb2c08bf
JZouhair <jawadi.zouhair.2017@gmail.com>
2018-09-06 15:51:30 +0100

    back of project store

---
+++ storeBack/src/test/java/org/demo/com/Service/TraitementMetierTest.java
@@ -0,0 +1,17 @@
+package org.demo.com.Service;
+
+import org.junit.Test;
+
+public class TraitementMetierTest {
+
+	@Test
+	public void testGetBookById() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetAllBooks() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
