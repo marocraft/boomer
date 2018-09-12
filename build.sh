e625d6311586a41aa7f10d613ddebab9276ee0ed
Nourelhouda1995 <nbenaffoune@gmail.com>
2018-09-12 21:22:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] ; then
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]] ; then
       echo 'Checking Quality Gates'
       mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION}
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
